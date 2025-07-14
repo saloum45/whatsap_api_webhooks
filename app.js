075f8230e6a21b438d7616b5c7e423a306e6f1c7
saloum45 <saloumfall45@gmail.com>
2025-07-14 11:20:26 +0000

    in progress

--- app.js
+++ app.js
@@ -13,25 +13,25 @@ const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
 app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+    const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
-  if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
-    res.status(200).send(challenge);
-  } else {
-    res.status(403).end();
-  }
+    if (mode === 'subscribe' && token === verifyToken) {
+        console.log('WEBHOOK VERIFIED');
+        res.status(200).send(challenge);
+    } else {
+        res.status(403).end();
+    }
 });
 
 // Route for POST requests
 app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+    const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+    console.log(`\n\nWebhook received ${timestamp}\n`);
+    console.log(JSON.stringify(req.body, null, 2));
+    res.status(200).end();
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`\nListening on port ${port}\n`);
+    console.log(`\nListening on port ${port}\n`);
 });
